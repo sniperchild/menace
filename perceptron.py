1bd9523971e545b5db060dc22aaf377e46b264da
Jason Jibrail <jason.jibrail@gmail.com>
2024-09-10 22:36:18 +0100

    WIP: detail in perceptron - needs np arrays

--- perceptron.py
+++ perceptron.py
@@ -5,20 +5,35 @@ class Neuron:
     '''
     Individual neuron with activatio bis and backprop
     '''
-    def __init__(self) -> None:
+    def __init__(self,weight_size) -> None:
         # bias
         # weight
         self.bias = random.random()
+        self.weights = [random.random() for _ in weight_size]
+
+    def activate(self,inputs):
+        # multiply weights by inputs and add bias
+        output = self.bias + []
+
+    numpy muliply here
 
 class Layer:
     def __init__(self,num,size) -> None:
         if num == 0:
             #initial layer - no weights
-            self.neurons = [Neuron() for x in size]
+            self.neurons = [Neuron() for _ in size]
             self.input_layer = True
         else:
-            self.neurons = [Neuron() for x in size]
+            self.neurons = [Neuron() for _ in size]
             self.input_layer = False
+
+    def run(self,inputs):
+
+        for neuron in self.neurons:
+            output = neuron.activate(inputs)
+
+
+        return outputs
 
 
 class Perceptron:
@@ -34,6 +49,15 @@ class Perceptron:
         self.layers = [Layer(num,size) for num,size in enumerate(layers)]
 
 
+    def run(self,input):
+        
+        for layer in layers:
+            input = layer.run(input)
+
+        return input
+
+
+
 
 def main():
 
@@ -41,6 +65,13 @@ def main():
 
     p = Perceptron(9,16,9)
 
+    #run_the_network
+
+    inputs = [0,0,0,0,0,0,0,0]
+
+    outputs = p.run(inputs)
+
+    print(outputs)
 
 
 if __name__ == '__main__':
